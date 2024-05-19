b51cf315afe0bff784a3e6b7c05868dafe97566b
Ahmad Sajid <ahmad.sajid605@gmail.com>
2024-05-19 16:03:48 +0500

    1st commit

---
+++ marketing-website/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div style={{ 
+      height: '200px',
+      width: '90%',
+      backgroundColor: 'blueviolet',
+      display: 'flex',
+      flexdirection: 'column',
+      margin: '5em',
+      }}>
+      
+    <h1 style={{ color: 'black', 
+    textAlign: 'center', padding: '1em'}}>Title</h1>
+    </div>
+        </div>
+
+  );
+}
+
+export default App;
