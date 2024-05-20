d77799cbeae812e7e5d15a0d3e1143558cdf25eb
Ahmad Sajid <ahmad.sajid605@gmail.com>
2024-05-20 17:04:42 +0500

    2nd commit
    completed full template (marketing-website)

--- marketing-website/src/App.js
+++ marketing-website/src/App.js
@@ -1,22 +1,106 @@
-import logo from './logo.svg';
+import * as React from 'react'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
     <div style={{ 
-      height: '200px',
+      height: '150px',
       width: '90%',
       backgroundColor: 'blueviolet',
       display: 'flex',
       flexdirection: 'column',
-      margin: '5em',
+      margin: '1em',
+      position: 'relative',
       }}>
-      
-    <h1 style={{ color: 'black', 
-    textAlign: 'center', padding: '1em'}}>Title</h1>
-    </div>
+
+        <div style={{ color: 'white',
+        width: '100px',
+        height: '100px',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        margin: '-50px 0 0 -50px'}}>
+        <h1>Title</h1>
         </div>
+    </div>
+
+    <div className='nav'></div>
+
+    <div style={{ 
+      height: '20px',
+      width: '50%',
+      backgroundColor: 'blueviolet',
+      display: 'flex',
+      margin: '5px auto',   
+      }}>
+    </div>
+
+    <div style={{ 
+      height: '20px',
+      width: '50%',
+      backgroundColor: 'blueviolet',
+      display: 'flex',
+      margin: '5px auto',   
+      }}>
+    </div>
+
+    <div style={{ 
+      height: '150px',
+      width: '100%',
+      backgroundColor: 'white',
+      display: 'grid',
+      left: '20%',
+      margin: 'auto',
+      position: 'relative',
+      }}>
+
+        <div style={{ 
+                color: 'white',
+                backgroundColor: 'blueviolet',
+                width: '100px',
+                height: '100px',
+                position: 'absolute',
+                top: '50%',
+                left: '0%',
+                margin: '-50px 0 0 -50px'
+        }}>
+        <h1 className="text">Feature 1</h1>
+        </div>
+
+        <div style={{ 
+                color: 'white',
+                backgroundColor: 'blueviolet',
+                width: '100px',
+                height: '100px',
+                position: 'absolute',
+                top: '50%',
+                left: '30%',
+                margin: '-50px 0 0 -50px'
+        }}>
+        <h1 className='text'>Feature 2</h1>
+        </div>
+
+        <div style={{ 
+                color: 'white',
+                backgroundColor: 'blueviolet',
+                width: '100px',
+                height: '100px',
+                position: 'absolute',
+                top: '50%',
+                left: '60%',
+                margin: '-50px 0 0 -50px'
+        }}>
+        <h1 className='text'>Feature 3</h1>
+        </div>
+    </div>
+    
+    <div className='nav'></div>
+    <div className='button'>
+    <button type='button'>Contact Us</button>
+    </div>
+</div>
 
   );
 }
