38b9f1a3515212331e56a85a00386f5c84cfe62d
Ahmad Sajid <ahmad.sajid605@gmail.com>
2024-05-22 22:56:46 +0500

    update complete
    completed the prototype

--- conference-schedule/src/App.js
+++ conference-schedule/src/App.js
@@ -5,33 +5,44 @@ function App() {
   return (
     <div className="App">
 
+
+<div>Event Name</div>
+
+<div class='grid1'>
+  <div class='item1'></div>
+  <div class='item1'></div>
+  <div class='item1'></div>
+</div>
+
 <div style={{
   height: '200px',
   width: '90%',
   display: 'flex',
   margin: '50px auto',
   backgroundColor: 'blueviolet',
-  position: 'relative'
+  position: 'relative',
 }}>
 
-<div style={{  
-  height: '20px',
-  width: '50%',
-  display: 'flex',
-  margin: '50px auto',
-  backgroundColor: 'white',
-  position: 'relative'
-  }}></div>
+<div style={{margin: 'auto'}}>
+        <div>
+          <div style={{height: '20px', width: '250px', margin: '0px auto', backgroundColor: 'white'}}></div>
+          
+          <div style={{height: '20px', width: '250px', margin: '5px auto', backgroundColor: 'white'}}></div>
+          
+          <button type='button' style={{backgroundColor: 'blue', color: 'white'}}>Buy a Ticket</button>
+   
+        </div>
+      </div>
 
-  <div style={{  
-  height: '20px',
-  width: '50%',
-  display: 'flex',
-  margin: '80px auto',
-  backgroundColor: 'black',
-  position: 'absolute'
-  }}></div>
+</div>
 
+<div class="grid">
+  <div class="item">Speaker 1</div>
+  <div class="item">Speaker 2</div>
+  <div class="item">Speaker 3</div>
+  <div class="item">Speaker 4</div>
+  <div class="item">Speaker 5</div>
+  <div class="item">Speaker 6</div>
 </div>
 
     </div>
